ade30c26dfeaea0b9ef8d66f68b3628d4ce6bf9b
SilverSixx <phamluongdat231103@gmail.com>
2024-02-14 17:38:02 +0700

    fix: tailwin config

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,23 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 
+const NavBarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+          {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+            <NavBarItem title={item} key={index + item} />
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
